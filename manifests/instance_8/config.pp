bcd43237b51af96787b58a493f679a7355f8a18f
Dario Sestero <dsestero@gmail.com>
2018-07-24 17:11:53 +0200

    replaced hiera with lookup

--- manifests/instance_8/config.pp
+++ manifests/instance_8/config.pp
@@ -73,7 +73,7 @@ define jboss::instance_8::config (
   }
 
   # Directory for deployment of applicative properties, retrieved via lookup
-  $customConfigurationsModule = lookup('inva::custom_configurations_module', Optional[String], 'first', undef)
+  $customConfigurationsModule = lookup('inva::custom_configurations_module', Optional[Tuple[String]], 'first', undef)
 
   if $customConfigurationsModule != undef {
     $modulesFolder = "${jboss_inst_folder}/modules/system/layers/base/"
